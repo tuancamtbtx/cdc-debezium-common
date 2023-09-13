496daefcf828ca1be7171083f6cfb45297b52bed
Tuan.Nguyen3 <tuan.nguyen3@trustingsocial.com>
2023-09-13 09:56:50 +0700

    [add] isbn converter debezium

--- cdc-debezium-converter/src/main/java/vn/dataplatform/cdc/converter/IsbnConverter.java
+++ cdc-debezium-converter/src/main/java/vn/dataplatform/cdc/converter/IsbnConverter.java
@@ -8,16 +8,16 @@ import java.util.Properties;
 /**
  * @author tuan.nguyen3
  */
-public class TimeConverter implements CustomConverter<SchemaBuilder, RelationalColumn> {
+public class IsbnConverter implements CustomConverter<SchemaBuilder, RelationalColumn> {
     private SchemaBuilder schemaBuilder;
     @Override
     public void configure(Properties props) {
-        schemaBuilder = SchemaBuilder.string().name(props.getProperty("name"));
+        schemaBuilder = SchemaBuilder.string().name(props.getProperty("schema.name"));
     }
 
     @Override
     public void converterFor(RelationalColumn field, ConverterRegistration<SchemaBuilder> registration) {
-        if("time".equals(field.typeName())) {
+        if("isbn".equals(field.typeName())) {
             registration.register(schemaBuilder, x -> x.toString());
         }
     }
